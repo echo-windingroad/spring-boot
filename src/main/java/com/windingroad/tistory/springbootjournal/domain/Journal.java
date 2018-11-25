9d244fec9305c788e46efd3ba8eb856fc2c71050
windingroadss <ked9181@naver.com>
2018-11-26 00:05:23 +0900

    @Transient error solved -  org.springframework.data.annotation.Transient → java.text.SimpleDateFormat

--- src/main/java/com/windingroad/tistory/springbootjournal/domain/Journal.java
+++ src/main/java/com/windingroad/tistory/springbootjournal/domain/Journal.java
@@ -1,11 +1,8 @@
 package com.windingroad.tistory.springbootjournal.domain;
 
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Entity
@@ -19,14 +16,14 @@ public class Journal {
     private Date created;
     private String summary;
 
-//    @Transient
-//    private SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+    @Transient
+    private SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
 
     public Journal(String title, String summary, String date)
             throws ParseException {
         this.title = title;
         this.summary = summary;
-//        this.created = format.parse(date);
+        this.created = format.parse(date);
     }
 
     Journal() {
@@ -64,9 +61,9 @@ public class Journal {
         this.summary = summary;
     }
 
-//    public String getCreatedAsShort() {
-//        return format.format(created);
-//    }
+    public String getCreatedAsShort() {
+        return format.format(created);
+    }
 
     public String toString() {
         StringBuilder value = new StringBuilder("JournalEntry(");
@@ -76,8 +73,8 @@ public class Journal {
         value.append(title);
         value.append(",요약: ");
         value.append(summary);
-//        value.append(",작성일자: ");
-//        value.append(getCreatedAsShort());
+        value.append(",작성일자: ");
+        value.append(getCreatedAsShort());
         value.append(")");
         return value.toString();
     }
