cd51d70af0b1375ed7208e5e728ccb5a8e5a3a4f
windingroadss <ked9181@naver.com>
2018-11-27 19:44:39 +0900

    Studying properties and profiles

--- ch03/spring-boot-config/src/main/java/com/tistory/windingroad/springname/springbootconfig/SpringBootConfigApplication.java
+++ ch03/spring-boot-config/src/main/java/com/tistory/windingroad/springname/springbootconfig/SpringBootConfigApplication.java
@@ -1,7 +1,12 @@
 package com.tistory.windingroad.springname.springbootconfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootConfigApplication {
@@ -9,4 +14,23 @@ public class SpringBootConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootConfigApplication.class, args);
 	}
+
+	private static Logger log = LoggerFactory.getLogger(SpringBootConfigApplication.class);
+
+	// mvn spring-boot:run -Dserver.ip=192.168.12.1
+	@Value("${server.ip}")
+	String serverIp;
+
+//	@Autowired
+//	MyAppProperties props;
+
+	@Bean
+	CommandLineRunner values() {
+		return args -> {
+			log.info(" > 서버 IP: " + serverIp);
+//			log.info(" > 애플리케이션 명: " + props.getName());
+//			log.info(" > 애플리케이션 정보: " + props.getDescription());
+		};
+	}
+
 }
