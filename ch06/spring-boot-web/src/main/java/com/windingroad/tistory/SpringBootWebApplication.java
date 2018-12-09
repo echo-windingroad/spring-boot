8e1f740849433785a3289b2f639a38324812db15
windingroadss <ked9181@naver.com>
2018-12-09 17:15:04 +0900

    Generating ch06

---
+++ ch06/spring-boot-web/src/main/java/com/windingroad/tistory/SpringBootWebApplication.java
@@ -0,0 +1,12 @@
+package com.windingroad.tistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
+}
